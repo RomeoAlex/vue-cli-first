27d353302553501b0a650de0c953cea9d5d0c2eb
RomeoAlex <romeo.alessandro17@gmail.com>
2021-12-17 15:48:39 +0100

    finalizzazione esercizio con bonus vue-cli-first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
+    <!-- riutilizzo l'immagine -->
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Title msg="La mia prima app con Vue CLI" />
+    <Hero/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Title from "./components/Title.vue";
+import Hero from "./components/Hero.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Title,
+    Hero,
   },
 };
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="Jumbotron">
+        <button>
+            Applicazione CLIVUE
+        </button>
+    </div>
+  
+</template>
+
+<script>
+// oggetto
+export default {
+  name: "Hero",
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+*{
+    margin: 0;
+    padding:0;
+    box-sizing: border-box;
+}
+.Jumbotron{
+    background-image: url(../assets/aliens-fireteam-elite-1276397-1280x0-min.jpeg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    width:100%;
+    height: 1000px;
+}
+
+</style>
